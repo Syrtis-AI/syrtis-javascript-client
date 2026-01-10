4963bae146bcd31e254903438375ad2c5cc3d45e
weeger <romain.weeger@gmail.com>
2026-01-10 10:28:52 +0100

    Get documents

--- src/Repository/DocumentRepository.ts
+++ src/Repository/DocumentRepository.ts
@@ -1,8 +1,22 @@
 import AbstractApiRepository from "@wexample/js-api/Common/AbstractApiRepository";
+import { type ApiEntityData } from "@wexample/js-api/Common/AbstractApiEntity";
 import Document from "../Entity/Document";
 
 export default class DocumentRepository extends AbstractApiRepository<Document> {
   static getEntityType() {
     return Document;
+  }
+
+  async fetchList(): Promise<Document[]> {
+    const response = await this.client.get(this.buildPath('list')).json<{
+      data?: { items?: ApiEntityData[] };
+    }>();
+    const items = response?.data?.items ?? [];
+
+    if (!Array.isArray(items)) {
+      return [];
+    }
+
+    return this.createFromApiCollection(items);
   }
 }
