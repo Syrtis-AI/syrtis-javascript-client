a9ce14d18c18b25a7d2c1d54eac5223b31ad5947
weeger <romain.weeger@gmail.com>
2026-01-10 09:06:23 +0100

    Api client

--- src/Common/SyrtisClient.ts
+++ src/Common/SyrtisClient.ts
@@ -1,6 +1,6 @@
-import ApiClient from "@wexample/js-api/Common/ApiClient";
+import AbstractApiClient from "@wexample/js-api/Common/AbstractApiClient";
 
-export default class SyrtisClient extends ApiClient {
+export default class SyrtisClient extends AbstractApiClient {
   static readonly DEFAULT_BASE_URL = 'https://api.syrtis.ai/api/';
 
   constructor(baseUrl: string | null = SyrtisClient.DEFAULT_BASE_URL) {
