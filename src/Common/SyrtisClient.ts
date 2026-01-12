fd71d65ee53fb40fc022053078c951dd90429a3a
weeger <romain.weeger@gmail.com>
2026-01-12 11:12:14 +0100

    Factorize

--- src/Common/SyrtisClient.ts
+++ src/Common/SyrtisClient.ts
@@ -1,8 +1,19 @@
+import AbstractApiEntitiesClient from "@wexample/js-api/Common/AbstractApiEntitiesClient";
 import { type ApiClientOptions } from "@wexample/js-api/Common/AbstractApiClient";
-import SyrtisEntitiesClient from "./SyrtisEntitiesClient";
+import { type RepositoryClass } from "@wexample/js-api/Common/ApiEntityManager";
+import DocumentRepository from "../Repository/DocumentRepository";
 
-export default class SyrtisClient extends SyrtisEntitiesClient {
+export default class SyrtisClient extends AbstractApiEntitiesClient {
+  static readonly DEFAULT_BASE_URL = 'https://api.syrtis.ai/api/';
+
   constructor(options: ApiClientOptions = {}) {
-    super(options);
+    super({
+      ...options,
+      baseUrl: options.baseUrl ?? SyrtisClient.DEFAULT_BASE_URL,
+    });
+  }
+
+  protected getRepositoryClasses(): RepositoryClass[] {
+    return [DocumentRepository];
   }
 }
