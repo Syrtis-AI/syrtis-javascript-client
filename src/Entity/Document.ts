6ba65f841cc085e017d34392864f1a3c60b374a9
weeger <romain.weeger@gmail.com>
2026-01-14 17:11:24 +0100

    Document info

--- src/Entity/Document.ts
+++ src/Entity/Document.ts
@@ -4,9 +4,15 @@ export default class Document extends AbstractApiEntity {
   static readonly entityName = 'document';
 
   title?: string;
+  projectSecureId?: string;
 
   constructor(data: ApiEntityData = {}) {
     super(data);
     this.title = data['title'] as string | undefined;
+    this.projectSecureId = data['project'] as string | undefined;
+  }
+
+  getProjectSecureId(): string | undefined {
+    return this.projectSecureId;
   }
 }
