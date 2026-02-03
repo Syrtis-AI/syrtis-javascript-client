f8ea4186f38a7d743772144525f3435293cf0e79
weeger <romain.weeger@gmail.com>
2026-02-03 18:57:41 +0100

    Rework api clients

--- src/Common/SyrtisClient.ts
+++ src/Common/SyrtisClient.ts
@@ -4,6 +4,7 @@ import type { RepositoryClass } from '@wexample/js-api/Common/ApiEntityManager';
 import ScenarioRepository from '../Repository/ScenarioRepository.js';
 import SessionRepository from '../Repository/SessionRepository.js';
 import UserRepository from '../Repository/UserRepository.js';
+import getEntitySchemas from './entitySchemas.js';
 
 export default class SyrtisClient extends AbstractApiEntitiesClient {
   static readonly DEFAULT_BASE_URL = 'https://api.syrtis.ai/api/';
@@ -17,5 +18,9 @@ export default class SyrtisClient extends AbstractApiEntitiesClient {
 
   protected getRepositoryClasses(): RepositoryClass[] {
     return [ScenarioRepository, SessionRepository, UserRepository];
+  }
+
+  getEntitySchemas(): Record<string, unknown> {
+    return getEntitySchemas();
   }
 }
---
+++ src/Common/entitySchemas.ts
@@ -0,0 +1,13 @@
+import scenario from '../data/entity/scenario.json';
+import session from '../data/entity/session.json';
+import user from '../data/entity/user.json';
+
+type EntitySchema = { name: string };
+
+export default function getEntitySchemas(): Record<string, EntitySchema> {
+  return {
+    [scenario.name]: scenario,
+    [session.name]: session,
+    [user.name]: user,
+  };
+}
