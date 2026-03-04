8f237c10173f975d9c30c194f5a4233330e6dba9
weeger <romain.weeger@gmail.com>
2026-03-04 13:23:14 +0100

    entity format

--- src/Vue/Mixin/Entity/message.vue
+++ src/Vue/Mixin/Entity/message.vue
@@ -1,8 +1,10 @@
 <script>
 import AbstractEntitySingleMixin from "@wexample/js-api/Vue/AbstractEntitySingleMixin";
 import MessageEntityManipulatorVueMixin from '../EntityManipulator/MessageEntityManipulatorVueMixin';
+import Entity from '@wexample/symfony-design-system/vue/bases/entity';
 
 export default {
+  extends: Entity,
   mixins: [AbstractEntitySingleMixin, MessageEntityManipulatorVueMixin],
 };
 </script>
--- src/Vue/Mixin/Entity/session.vue
+++ src/Vue/Mixin/Entity/session.vue
@@ -1,8 +1,10 @@
 <script>
 import AbstractEntitySingleMixin from "@wexample/js-api/Vue/AbstractEntitySingleMixin";
 import SessionEntityManipulatorVueMixin from '../EntityManipulator/SessionEntityManipulatorVueMixin';
+import Entity from '@wexample/symfony-design-system/vue/bases/entity';
 
 export default {
+  extends: Entity,
   mixins: [AbstractEntitySingleMixin, SessionEntityManipulatorVueMixin],
 };
 </script>
